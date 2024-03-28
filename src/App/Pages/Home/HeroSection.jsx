de40aa6fb626f0d184ff8ce0d550ab015fc4562a
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-03-28 22:06:41 +0300

    Done with Hero Section

--- src/App/Pages/Home/HeroSection.jsx
+++ src/App/Pages/Home/HeroSection.jsx
@@ -3,21 +3,21 @@ export default function HeroSection() {
       <section id="heroSection" className="hero--section">
         <div className="hero--section--content--box">
           <div className="hero--section--content">
-            <p className="section--title">Hey, I'm John</p>
+            <p className="section--title">Hey, I'm Trevor Madara Kayeyia</p>
             <h1 className="hero--section--title">
               <span className="hero--section-title--color">Full Stack</span>{" "}
               <br />
               Developer
             </h1>
             <p className="hero--section-description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              <br /> Dolorum, quas. Amet soluta assumenda cum?
+             I am a  tech enthusiast, both web based and mobile apps
+              <br /> Wanna Engage Me?
             </p>
           </div>
           <button className="btn btn-primary">Get In Touch</button>
         </div>
         <div className="hero--section--img">
-          <img src="./img/hero_img.png" alt="Hero Section" />
+          <img src="./img/hero_img.jpeg" alt="Hero Section" />
         </div>
       </section>
     );
